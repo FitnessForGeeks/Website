5976cd2cdb2e6ac449f05f58f2158209cfbe8cc5
TimUntersberger <timuntersberger2@gmail.com>
2018-04-19 17:16:38 +0200

    fixed conflict

---
+++ store/account.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+    loggedIn: false
+})
+
+export const mutations = {
+    logIn(state){
+        state.loggedIn = true;
+    },
+    logOut(state){
+        state.loggedIn = false;
+    }
+}
